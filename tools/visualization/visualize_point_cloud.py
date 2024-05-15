d73d3f547997b647c7a290625deac0581af65c34
yokosyun <yoko.syun.1408@gmail.com>
2024-05-15 13:49:21 +0900

    set MAX_DIST, GRID_SIZE

--- tools/visualization/visualize_point_cloud.py
+++ tools/visualization/visualize_point_cloud.py
@@ -1,21 +1,60 @@
 import numpy as np
-import torch
 
-from mmdet3d.structures import LiDARInstance3DBoxes
-from mmdet3d.visualization import Det3DLocalVisualizer
+import open3d as o3d
+
+
+NUM_DIMENSIONS = 2
+GRID_SIZE = 1.0
+MAX_DIST = 100
+
+
+def gen_grid(grid_size: float, max_value: float) -> o3d.geometry.LineSet:
+
+    line_set = o3d.geometry.LineSet()
+
+    # Y dimension
+    x = np.arange(-max_value, max_value + grid_size, grid_size)
+    x = np.repeat(x, NUM_DIMENSIONS)
+    y = np.full_like(x, -max_value)
+    y[::NUM_DIMENSIONS] = max_value
+    z = np.zeros_like(x)
+    points_Y = np.vstack((x, y, z)).T
+
+    # X dimension
+    y = np.arange(-max_value, max_value + grid_size, grid_size)
+    y = np.repeat(y, NUM_DIMENSIONS)
+    x = np.full_like(y, -max_value)
+    x[::NUM_DIMENSIONS] = max_value
+    z = np.zeros_like(y)
+    points_X = np.vstack((x, y, z)).T
+
+    # XYZ dimension
+    points = np.vstack((points_X, points_Y))
+    line_set.points = o3d.utility.Vector3dVector(points)
+    lines = np.arange(points.shape[0]).reshape(-1, NUM_DIMENSIONS)
+    line_set.lines = o3d.utility.Vector2iVector(lines)
+
+    line_set.paint_uniform_color((0.5, 0.5, 0.5))
+
+    return line_set
+
 
 points = np.fromfile(
-    'demo/data/nuscenes/n015-2018-07-24-11-22-45+0800__LIDAR_TOP__1532402927647951.pcd.bin',
+    "demo/data/nuscenes/n015-2018-07-24-11-22-45+0800__LIDAR_TOP__1532402927647951.pcd.bin",
     dtype=np.float32,
 )
-print(points.shape)
 points = points.reshape(-1, 5)
-visualizer = Det3DLocalVisualizer()
-# set point cloud in visualizer
-visualizer.set_points(points)
-# bboxes_3d = LiDARInstance3DBoxes(
-#     torch.tensor([[8.7314, -1.8559, -1.5997, 4.2000, 3.4800, 1.8900, -1.5808]])
-# )
-# Draw 3D bboxes
-# visualizer.draw_bboxes_3d(bboxes_3d)
-visualizer.show()
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(points[:, :3])
+line_set_grid = gen_grid(grid_size=GRID_SIZE, max_value=MAX_DIST)
+
+voxel_pcd = o3d.geometry.VoxelGrid.create_from_point_cloud(pcd, voxel_size=0.5)
+# print(type(voxel_pcd.property))
+# # voxel_pcd[:, 3] += 1.0
+o3d.visualization.draw_geometries(
+    [pcd, line_set_grid, voxel_pcd],
+    zoom=0.3412,
+    front=[0.4257, -0.2125, -0.8795],
+    lookat=[2.6172, 2.0475, 1.532],
+    up=[-0.0694, -0.9768, 0.2024],
+)
