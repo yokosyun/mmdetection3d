b8569dc939cde4a0f924c8eab681635a42b46baa
yokosyun <yoko.syun.1408@gmail.com>
2024-10-20 05:29:15 +0900

    increase batch size

--- configs/centerpoint/centerpoint_pillar02_second_secfpn_8xb4-cyclic-20e_nus-3d_radar.py
+++ configs/centerpoint/centerpoint_pillar02_second_secfpn_8xb4-cyclic-20e_nus-3d_radar.py
@@ -145,7 +145,7 @@ test_pipeline = [
 
 train_dataloader = dict(
     _delete_=True,
-    batch_size=2,
+    batch_size=16,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
